d407be837d4b5f585be9aad95498622c8f15ffda
Ilya K <5115869-karakulov.ii@users.noreply.gitlab.com>
2021-09-07 15:25:22 +0500

    Added tests for ProviderCB.branchTaskHandler()

--- __tests__/IProvider/ProviderCB.test.ts
+++ __tests__/IProvider/ProviderCB.test.ts
@@ -193,8 +193,198 @@ describe('resumeTaskHandler()', () => {
 })
 
 describe('branchTaskHandler()', () => {
+    it('If task.data.kind !== "branch", the method must throw a specific exception', async () => {
+        expect.assertions(1);
 
+        const task:TaskFormat = {
+            data: {
+                kind: 'resume',
+                resumeID: "R000000000000000000"
+            }
+        };
+
+        const provCB = new ProviderCB(
+            new Tasks(),
+            new CBAccountPoolDB(),
+            new LogDB());
+
+        try {
+            // @ts-ignore
+            await provCB.branchTaskHandler(task);
+        } catch (e) {
+            expect(e.message).toContain('Error the task type');
+        }
+    })
+
+    it('If it crashes when running accountPool.getResumeList(task), task must be resent to the queue', async () => {
+        expect.assertions(1);
+
+        const task:TaskFormat = {
+            data: {
+                kind: 'branch',
+                keywords: 'driver OR cashier',
+                city: 'Atlanta',
+                state: 'GA',
+                freshness: 10,
+                page: 1,
+                rowsPerPage: 10
+            }
+        };
+
+        const provCB = new ProviderCB(
+            new Tasks(),
+            new CBAccountPoolDB(),
+            new LogDB());
+
+        // @ts-ignore
+        provCB.accountPool.getResumeList = jest.fn()
+            .mockRejectedValue(new Error('Something error'));
+
+        // @ts-ignore
+        const putTasksListing = provCB.tasks.putTasksListing = jest.fn()
+
+        try {
+            // @ts-ignore
+            await provCB.branchTaskHandler(task)
+        } catch {
+            // empty
+        }
+        finally {
+            // @ts-ignore
+            expect(putTasksListing).toHaveBeenLastCalledWith(task.data.city, task.data.state, task.data.page)
+        }
+    })
+
+    it('If it have got resumes list, then must be created tasks to parse these resumes', async () => {
+        expect.assertions(2);
+
+        const task:TaskFormat = {
+            data: {
+                kind: 'branch',
+                keywords: 'driver OR cashier',
+                city: 'Atlanta',
+                state: 'GA',
+                freshness: 10,
+                page: 8,
+                rowsPerPage: 10
+            }
+        };
+
+        const provCB = new ProviderCB(
+            new Tasks(),
+            new CBAccountPoolDB(),
+            new LogDB());
+
+        // @ts-ignore
+        provCB.accountPool.getResumeList = jest.fn(task => {
+            if (task.data.kind !== 'branch') throw new Error('It isn\'t a branch');
+
+            return Promise.resolve({
+                page: task.data.page,
+                maxPage: 10,
+                resumes: [
+                    'R000000000',
+                    'R000000001',
+                    'R000000002',
+                    'R000000003',
+                    'R000000004',
+                    'R000000005',
+                    'R000000006',
+                    'R000000007',
+                    'R000000008',
+                ]
+            })
+        });
+
+        // @ts-ignore
+        const putTasksResumes = provCB.tasks.putTasksResumes = jest.fn()
+
+        try {
+            // @ts-ignore
+            await provCB.branchTaskHandler(task)
+        } catch {
+            // empty
+        }
+        finally {
+            // @ts-ignore
+            expect(putTasksResumes.mock.calls[0][0][0]).toBe('R000000000');
+            expect(putTasksResumes.mock.calls[0][0][8]).toBe('R000000008');
+        }
+    })
+
+    it('If page == 1 and maxPage > 1 then the method must be created tasks to parse pages 2..maxPage', async () => {
+        expect.assertions(5);
+
+        const task:TaskFormat = {
+            data: {
+                kind: 'branch',
+                keywords: 'driver OR cashier',
+                city: 'Atlanta',
+                state: 'GA',
+                freshness: 10,
+                page: 1,
+                rowsPerPage: 10
+            }
+        };
+
+        const provCB = new ProviderCB(
+            new Tasks(),
+            new CBAccountPoolDB(),
+            new LogDB());
+
+        const maxPage = 10;
+
+        // @ts-ignore
+        provCB.accountPool.getResumeList = jest.fn(task => {
+            if (task.data.kind !== 'branch') throw new Error('It isn\'t a branch');
+
+            return Promise.resolve({
+                page: task.data.page,
+                maxPage: maxPage,
+                resumes: [
+                    'R000000000',
+                    'R000000001',
+                    'R000000002',
+                    'R000000003',
+                    'R000000004',
+                    'R000000005',
+                    'R000000006',
+                    'R000000007',
+                    'R000000008',
+                ]
+            })
+        });
+
+        // @ts-ignore
+        provCB.tasks.putTasksResumes = jest.fn()
+
+        // @ts-ignore
+        const putTasksListing = provCB.tasks.putTasksListing = jest.fn()
+
+        try {
+            // @ts-ignore
+            await provCB.branchTaskHandler(task)
+        } catch {
+            // empty
+        }
+        finally {
+            expect(putTasksListing).toHaveBeenCalledTimes(maxPage - 1);
+
+            // @ts-ignore
+            expect(putTasksListing.mock.calls[0][0]).toBe(task.data.city);
+
+            // @ts-ignore
+            expect(putTasksListing.mock.calls[0][1]).toBe(task.data.state);
+
+            // @ts-ignore
+            expect(putTasksListing.mock.calls[0][2]).toBe(2);
+
+            // @ts-ignore
+            expect(putTasksListing.mock.calls[maxPage - 2][2]).toBe(maxPage);
+        }
+    })
 })
+
 describe('getResumes()', () => {
     it('The method clearResumes() must be launch after give the resumes', async () => {
         expect.assertions(1);
