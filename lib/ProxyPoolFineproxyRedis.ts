5dbeb108b2cf590d13010f284fed3881fd2b5b06
Ilya K <5115869-karakulov.ii@users.noreply.gitlab.com>
2021-09-09 17:03:28 +0500

    Added class ProxyPoolFineproxyRedis, extends the class ProxyPoolFineproxy

---
+++ lib/ProxyPoolFineproxyRedis.ts
@@ -0,0 +1,33 @@
+import ProxyPoolFineproxy from "./ProxyPoolFineproxy";
+import Redis from "./Redis";
+
+const rcli = new Redis();
+
+export default class ProxyPoolFineproxyRedis extends ProxyPoolFineproxy {
+
+    protected redis_key = 'proxy';
+
+    constructor(...args:string[]) {
+        super(...args);
+    }
+
+    protected async cleanList():Promise<void> {
+        await rcli.del(this.redis_key);
+    }
+
+    protected async addProxyToList(proxy: string): Promise<void> {
+        await rcli.lpush(this.redis_key, [ proxy ]);
+    }
+
+    public async badProxy(proxy: string):Promise<void> {
+        await rcli.lrem(this.redis_key, 1, proxy);
+    }
+
+    protected async proxyAmount(): Promise<number> {
+        return await rcli.llen(this.redis_key);
+    }
+
+    protected async getProxyFromStorage():Promise<string> {
+        return await rcli.rpoplpush(this.redis_key, this.redis_key);
+    }
+}
