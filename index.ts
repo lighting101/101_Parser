1876ff8b602de18135a1d63e6e4f222c0d604cd3
lighting101 <karakulov@pm.me>
2021-10-05 12:58:42 +0500

    Parallel parsing improved

--- index.ts
+++ index.ts
@@ -6,11 +6,13 @@ const log = LogFactory('MAIN');
 function runService():Promise<void> {
     return new Promise((resolve, reject) => {
         const worker = new Worker('./lib/ResumeProviders/CBChild.js', );
-        worker.on('message', resolve);
         worker.on('error', reject);
         worker.on('exit', (code) => {
-            if (code !== 0)
+            if (code !== 0) {
                 reject(new Error(`Worker stopped with exit code ${code}`));
+            } else {
+                resolve();
+            }
         })
     })
 }
@@ -31,5 +33,5 @@ setInterval(async () => {
         await log.info(`Loop finished`);
         processing = false;
     }
-}, 5000)
+}, 1000)
 
